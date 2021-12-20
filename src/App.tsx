88da31b1ab842ee11f2a815c284725d149d5fd5c
YuriyLaznyuk <yuriy.laznyuk@gmail.com>
2021-12-20 19:34:45 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import './app.scss';
+import im1 from './assets/icons/favicon.png';
+import NBU from './assets/icons/vector.svg';
+import Main from './components/Main/Main';
+
+const App: React.FC = () => {
+    const a = 10;
+    return (
+        <div className="wrapper">
+            <h1>React 17 and TypeScript 4 App! {a}</h1>
+            <div className="wrapper__img">
+                <img src={im1} alt="nbu" width={50} />
+
+                <NBU />
+            </div>
+            <Main />
+        </div>
+    );
+};
+
+export default App;
